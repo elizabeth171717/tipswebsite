589d49f6eca2559a2c51639df7c366e78623e13a
Elizabeth Teresa <69443070+elizabeth171717@users.noreply.github.com>
2023-11-14 19:20:43 -0500

    animation
    animation added

--- app.js
+++ app.js
@@ -9,3 +9,31 @@ gsap.to(".logo", {
         toogleActions:"restart"
         }
     })
+
+
+
+    //images animation
+    const boxes = 
+document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+    const triggerBottom =
+    window.innerHeight / 5 * 4;
+
+
+boxes.forEach((box) =>{
+    const boxTop = box.getBoundingClientRect().top;
+
+    if(boxTop < triggerBottom){
+        box.classList.add('show');
+} else {
+    box.classList.remove('show');
+}
+
+});
+
+}
