8e37b20cee5d5e3ac17bf6829dc5ccae85d65277
Elizabeth Teresa <69443070+elizabeth171717@users.noreply.github.com>
2024-06-02 23:29:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.to(".logo", {
-    x: 150,
-    rotation:360,
-    duration:2,
-    ScrollTrigger: {
-        trigger: ".logo",
-        toogleActions:"restart"
-        }
-    })
 
